2afc5cbc64e12688ee7f237f4a371d4504f58b3a
jigangz <zjg0907008@gmail.com>
2024-02-26 21:20:21 -0800

    all done expect bug

--- Assets/Scripts/RockDestory.cs
+++ Assets/Scripts/RockDestory.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class RockDestroy : ToolHit
 {
-    public GameObject lampPrefab; 
-    public bool isSpecial = false; 
+    public GameObject lampPrefab;
+    public GameObject cavePrefab;
+    public GameObject holePrefab;
+    public bool isSpecial = false;
+    public bool isSuper = false;
+    public bool isSpicy = false;
 
     public override void Hit()
     {
@@ -14,7 +18,17 @@ public class RockDestroy : ToolHit
             
             Instantiate(lampPrefab, transform.position, Quaternion.identity);
         }
-        
+        if (isSuper)
+        {
+
+            Instantiate(cavePrefab, transform.position, Quaternion.identity);
+        }
+        if (isSpicy)
+        {
+
+            Instantiate(holePrefab, transform.position, Quaternion.identity);
+        }
+
         Destroy(gameObject);
     }
 }
