7df900142b4ecec423c36a5ae68e022b9b37575e
jigangz <zjg0907008@gmail.com>
2024-02-26 17:26:29 -0800

    111

--- Assets/Scripts/DialogManager.cs
+++ Assets/Scripts/DialogManager.cs
@@ -1,29 +1,36 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-using System.Collections.Generic;
 
 public class DialogManager : MonoBehaviour
 {
-    public GameObject dialogPanel; // The dialog box panel
-    public Text dialogText; // The text component that displays the dialog
-    public GameObject optionsPanel;  // The panel containing option buttons
-    public Button option1Button; // Option 1 button
-    public Button option2Button; // Option 2 button
+    public GameObject dialogPanel;
+    public Text dialogText;
+    public GameObject optionsPanel;
+    public Button option1Button;
+    public Button option2Button;
 
-    private Queue<string> sentences; // Queue to store dialogue sentences
-    private bool isDisplayingDialogue = false; // Flag to check if dialogue is currently being displayed
-
+    private Queue<string> sentences;
+    public bool isDisplayingDialogue = false;
 
     void Start()
     {
         sentences = new Queue<string>();
         dialogPanel.SetActive(false);
         optionsPanel.SetActive(false);
+
+        string[] initialDialogLines = new string[] {
+            "Where am I...? I think it will be wise for me to find that lamp. I think it will become useful. ...",
+            "HELP ALAD-WIN FIND THE LAMP! USE ARROW KEYS TO MOVE AND SPACEBAR TO INTERACT WITH AN OBJECT. LET¡¯S START WITH GETTING THE PICKAXE. ...",
+            "Great! now you can use a pickaxe to break the rocks! Break the rocks to find a way out! "
+        };
+
+        TriggerInitialDialog(initialDialogLines);
     }
 
-    public void StartDialog(string[] dialogLines)
+    public void TriggerInitialDialog(string[] dialogLines)
     {
         sentences.Clear();
         foreach (string line in dialogLines)
@@ -32,7 +39,7 @@ public class DialogManager : MonoBehaviour
         }
 
         dialogPanel.SetActive(true);
-        isDisplayingDialogue = true; // Start displaying dialogue
+        isDisplayingDialogue = true;
         DisplayNextSentence();
     }
 
@@ -40,8 +47,7 @@ public class DialogManager : MonoBehaviour
     {
         if (sentences.Count == 0)
         {
-            StartCoroutine(ShowOptionsAfterDelay(1));  // Wait 1 second after dialogue ends before showing options
-            isDisplayingDialogue = false;  // Stop displaying dialogue
+            EndDialogue();
             return;
         }
 
@@ -49,15 +55,23 @@ public class DialogManager : MonoBehaviour
         dialogText.text = sentence;
     }
 
-    IEnumerator ShowOptionsAfterDelay(float delay)
+    void EndDialogue()
     {
-        yield return new WaitForSeconds(delay);
-        ShowOptions();
+        isDisplayingDialogue = false;
+        dialogPanel.SetActive(false);
+        // ShowOptions is now removed from here
+    }
+
+    public void ShowOptionsAfterInteraction()
+    {
+        if (!isDisplayingDialogue)
+        {
+            ShowOptions();
+        }
     }
 
     void ShowOptions()
     {
-        dialogPanel.SetActive(false); // Ensure the dialogue box is hidden when options are shown
         optionsPanel.SetActive(true);
         option1Button.onClick.RemoveAllListeners();
         option2Button.onClick.RemoveAllListeners();
@@ -65,32 +79,35 @@ public class DialogManager : MonoBehaviour
         option1Button.onClick.AddListener(delegate { ChooseOption(1); });
         option2Button.onClick.AddListener(delegate { ChooseOption(2); });
 
-        option1Button.GetComponentInChildren<Text>().text = "Choose Forgiveness";
-        option2Button.GetComponentInChildren<Text>().text = "Choose Kill";
+        option1Button.GetComponentInChildren<Text>().text = "Kill";
+        option2Button.GetComponentInChildren<Text>().text = "Forgiveness";
     }
 
     public void ChooseOption(int option)
     {
-        dialogPanel.SetActive(false); // Ensure the dialogue box is hidden again
-        optionsPanel.SetActive(false); // Hide the options panel
+        optionsPanel.SetActive(false);
 
         switch (option)
         {
             case 1:
-                SceneManager.LoadScene("Maze");
+                SceneManager.LoadScene("SceneNameForOption1");
                 break;
             case 2:
-                SceneManager.LoadScene("Scene2");
+                SceneManager.LoadScene("Maze");
                 break;
         }
     }
 
     void Update()
     {
-        // Check if the user has clicked the left mouse button or pressed the space bar, and ensure we are currently displaying dialogue
         if (isDisplayingDialogue && (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space)))
         {
             DisplayNextSentence();
         }
     }
+    public bool IsDisplayingDialogue
+    {
+        get { return isDisplayingDialogue; }
+    }
+
 }
--- Assets/Scripts/Lamp.cs
+++ Assets/Scripts/Lamp.cs
@@ -1,19 +1,61 @@
 using UnityEngine;
 
-public class LanternScript : MonoBehaviour
+public class Lamp : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D collision)
+    private bool hasInteracted = false; // 确保交互只发生一次
+    private bool isPlayerNear = false; // 检测玩家是否在灯笼附近
+
+    private void OnTriggerEnter(Collider other)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (other.CompareTag("Player")) // 检测玩家标签
         {
-            
-            string[] dialogLines = new string[] {
-                "Hello, adventurer!",
-                "Do you wish to proceed?",
-                "HAHA What is your wish" };
-            
-            FindObjectOfType<DialogManager>().StartDialog(dialogLines);
+            isPlayerNear = true;
         }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerNear = false;
+            Debug.Log("Player entered lamp trigger zone.");
+        }
+    }
+
+    void Update()
+    {
+        // 玩家靠近并按下空格键时进行互动
+        if (isPlayerNear && Input.GetKeyDown(KeyCode.Space) && !hasInteracted)
+        {
+            InteractWithLamp();
+
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Space key pressed.");
+        }
+
+        // 如果已经与灯笼交互且对话结束，显示选项
+        if (hasInteracted && !FindObjectOfType<DialogManager>().isDisplayingDialogue)
+        {
+            FindObjectOfType<DialogManager>().ShowOptionsAfterInteraction();
+        }
+    }
+
+    void InteractWithLamp()
+    {
+        Debug.Log("Interacting with lamp...");
+        hasInteracted = true;
+        string[] dialogLinesAfterLamp = new string[] {
+            "oh hey you found me! ",
+            "WHo are you??? ",
+            "I am the Genie of the lamp! I am guessing that there’s a movie made about me... so I am assuming that you know me. ",
+            "So i’ll skip introduction. Just so you know I can’t help you get out of this cave. ",
+            "WHAT?? That’s a bad news....  ",
+            "yeah, yeah, uh, but what I can do is that I can help you destroy stuff? Like do you want to kill that guy who put you here? "
+        };
+
+        FindObjectOfType<DialogManager>().TriggerInitialDialog(dialogLinesAfterLamp);
     }
 }
 
